62afd492b0531eccbace1e39200133cbf409b0e9
Al3xiel <gabriel280104casas@gmail.com>
2024-09-22 17:04:09 -0500

    GAAA1

--- src/app/log-in/log/log-in-page.component.spec.ts
+++ src/app/log-in/log/log-in-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LogInPageComponent } from './log-in-page.component';
+import LogInPageComponent from './log-in-page.component';
 
 describe('LogInPageComponent', () => {
   let component: LogInPageComponent;
