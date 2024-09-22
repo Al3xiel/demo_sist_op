8aa0eba2b960f742fea13ad4ca5a5a8db6f35f7b
Al3xiel <gabriel280104casas@gmail.com>
2024-09-22 15:31:28 -0500

    GAAA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./log-in/log-in-routes.route')
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('./products/features/product-shell/product.route')
+  },
+  {
+    path: 'cart',
+    loadChildren: ()=> import('./cart/cart.route')
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
